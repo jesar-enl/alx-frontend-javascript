efee9fd4a16dddcc2ef53019dffadd0e7fbb0c7e
Jesse Kyambadde <jessekyambadde0@gmail.com>
2023-06-27 17:58:02 +0300

    modified file

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -5,7 +5,7 @@ export default function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()])
     .then((results) => {
       const [photoResponse, userResponse] = results;
-      console.log(`${photoResponse.body}  ${userResponse.firstName} ${userResponse.lastName}`);
+      console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
     })
     // eslint-disable-next-line no-unused-vars
     .catch((error) => {
