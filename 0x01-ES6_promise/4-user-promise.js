5ace03ba630ffc5d19950fd7d399df0938fdb971
Jesse Kyambadde <jessekyambadde0@gmail.com>
2023-06-27 18:08:15 +0300

    Simple promise

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,9 +1,8 @@
 // eslint-disable-next-line no-unused-vars
-import { createUser } from './utils';
 
 export default function signUpUser(firstName, lastName) {
   return Promise.resolve({
     firstName,
-    lastName
+    lastName,
   });
 }
