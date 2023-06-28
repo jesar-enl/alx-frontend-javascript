f4772f1f09b6a02dd3b4165873dc38253abfa975
Jesse Kyambadde <jessekyambadde0@gmail.com>
2023-06-28 09:33:24 +0300

    modified 3-all.js file

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,13 +2,13 @@
 import { createUser, uploadPhoto } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
+  return Promise.all([uploadPhoto(), createUser()])
     .then((results) => {
       const [photoResponse, userResponse] = results;
       console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
     })
     // eslint-disable-next-line no-unused-vars
-    .catch((error) => {
-      console.error('Signup system offline');
+    .catch(() => {
+      console.log('Signup system offline');
     });
 }
