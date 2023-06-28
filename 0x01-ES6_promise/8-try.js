1aa97efd1fb003a59229e0812226b25996b02376
Jesse Kyambadde <jessekyambadde0@gmail.com>
2023-06-28 11:12:55 +0300

    fixed bug

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,6 +1,7 @@
+/* eslint-disable linebreak-style */
 export default function divideFunction(numerator, denominator) {
   if (denominator === 0) {
-    throw Error('Cannot divide by 0');
+    throw Error('cannot divide by 0');
   }
   return numerator / denominator;
 }
