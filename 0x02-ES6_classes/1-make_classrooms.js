34c2bac7e960f0161504a1c12bdf349380e7876e
Jesse Kyambadde <jessekyambadde0@gmail.com>
2023-06-29 13:16:31 +0300

    fixed bug in function

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,13 +1,9 @@
 import ClassRoom from './0-classroom.js';
 
 export default function initializeRooms() {
-  const roomSizes = [19, 20, 34];
-  const rooms = [];
-
-  for (const size of roomSizes) {
-    const room = new ClassRoom(size);
-    rooms.push(room);
-  }
-
-  return rooms;
+  return [
+    new ClassRoom(19),
+    new ClassRoom(20),
+    new ClassRoom(34),
+  ];
 }
