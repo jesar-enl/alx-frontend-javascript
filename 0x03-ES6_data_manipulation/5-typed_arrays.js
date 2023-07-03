f00717ea45aa90cafe24f85d5a9e3b0f03b81add
Jesse Kyambadde <jessekyambadde0@gmail.com>
2023-07-03 19:20:33 +0300

    typed array sorted

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,10 +1,8 @@
 export default function createInt8TypedArray(length, position, value) {
-  if (position < 0 || position >= length)
-    throw new Error('Position outside range');
-
   const buffer = new ArrayBuffer(length);
-  const dataView = new DataView(buffer);
-  dataView.setInt8(position, value);
+  const intArray = new Int8Array(buffer);
 
-  return dataView;
+  if (position > intArray.length) throw new Error('Position outside range');
+  intArray[position] = value;
+  return new DataView(buffer);
 }
